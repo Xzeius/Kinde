144fe3b756b576d9c6900f705f09144ef2746d0e
Xzeius <rameshsuday@gmail.com>
2024-03-10 13:49:18 +0530

    dsa

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-// your_custom_script.js
-
+import createKindeClient from "@kinde-oss/kinde-auth-pkce-js";
 (async () => {
-    const kinde = await createKindeClient({
-        client_id: "3d39e4c74dda4a1abc434f23788b33fd",
-        domain: "https://noteshare.kinde.com",
-        redirect_uri: window.location.origin
-    });
+const kinde = await createKindeClient({
+client_id: "3d39e4c74dda4a1abc434f23788b33fd",
+domain: "https://noteshare.kinde.com",
+redirect_uri: window.location.origin
+});
+})
 
-    // Bind events to the login and register buttons
-    document.getElementById("login").addEventListener("click", async () => {
-        await kinde.login();
+document.getElementById("login").addEventListener("click", async () => {
+    await kinde.login();
     });
-
     document.getElementById("register").addEventListener("click", async () => {
-        await kinde.register();
+    await kinde.register();
     });
-})();
